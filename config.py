ceae8ca2b38e21ea5037026647291dbaeea656bc
Fredrik Kock <freko247@gmail.com>
2013-11-24 23:32:08 +0100

    Read configuration file using absolute path.

--- config.py
+++ config.py
@@ -3,8 +3,10 @@
 Configuration script
 """
 import ConfigParser
+import os
+
 
 config = ConfigParser.RawConfigParser()
-config.read('conflictMapper.cfg')
+config.read(os.path.join(os.path.dirname(__file__), 'conflictMapper.cfg'))
 
 DATABASE_LOCATION = config.get('database', 'DATABASE_LOCATION')
