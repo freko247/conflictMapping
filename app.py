72a6de0ed75b1086dfbf1bc4f7f5d58a95e13873
Fredrik Kock <freko247@gmail.com>
2013-11-25 19:34:00 +0100

    Added logging functionality to web server and added shutdown function for graceful shutdown.

--- app.py
+++ app.py
@@ -6,15 +6,28 @@ Usage:
   app.py
   app.py debug
 """
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import time
+import signal
 
-from sqlalchemy.orm import scoped_session, sessionmaker
-import tornado.ioloop
-import tornado.web
 from docopt import docopt
+from sqlalchemy.orm import scoped_session, sessionmaker
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
 
 from database import db
 from database.models import Tweet
 import config
+
+
+MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 3
+
+logger = None
+server = None
 
 
 class Application(tornado.web.Application):
@@ -54,16 +67,68 @@ class MainHandler(tornado.web.RequestHandler):
                    % len(tweets))
 
 
-if __name__ == ("__main__"):
-    arguments = docopt(__doc__)
+def sig_handler(sig, frame):
+    """
+    Method that handles received signals.
+    """
+    logger.warning('Caught signal: %s', sig)
+    tornado.ioloop.IOLoop.instance().add_callback(shutdown)
+
+
+def shutdown():
+    """
+    Method shuts down server when all existings requests have been processed,
+    or after maximum interval (set in configuration file).
+    """
+    logger.info('Stopping http server')
+    server.stop()
+
+    logger.info('Will shutdown in %s seconds ...',
+                config.WAIT_SECONDS_BEFORE_SHUTDOWN)
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    deadline = time.time() + config.WAIT_SECONDS_BEFORE_SHUTDOWN
+
+    def stop_loop():
+        """
+        Stop loop, checks for incoming requests and tries to shut down server.
+        """
+        now = time.time()
+        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+            io_loop.add_timeout(now + 1, stop_loop)
+        else:
+            io_loop.stop()
+            logger.info('Shutdown')
+    stop_loop()
+
+
+def main(arguments):
+    logHandler = TimedRotatingFileHandler("logs/app.log", when="midnight")
+    logFormatter = logging.Formatter('%(asctime)s: %(levelname)s; %(message)s')
+    logHandler.setFormatter(logFormatter)
+    global logger
+    logger = logging.getLogger('app logger')
+    logger.addHandler(logHandler)
+    logger.setLevel(logging.INFO)
+
     debug = arguments.get('debug')
     if debug:
         port = config.DEBUG_PORT
+        logger.info('Starting server in debug mode!')
         print 'Starting server in debug mode!'
     else:
         port = config.SERVER_PORT
-        print 'Starting server!'
-    print 'Tornado server ready at 127.0.0.1:%s' % port
+        logger.info('Starting server!')
+    logger.info('Tornado server ready at 127.0.0.1:%s (PID: %s)' %
+                (port, os.getpid()))
+    global server
     application = Application(debug=debug)
-    application.listen(port)
+    server = tornado.httpserver.HTTPServer(application)
+    server.listen(port)
+    signal.signal(signal.SIGTERM, sig_handler)
+    signal.signal(signal.SIGINT, sig_handler)
     tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == ("__main__"):
+    arguments = docopt(__doc__)
+    main(arguments)
--- config.py
+++ config.py
@@ -11,5 +11,9 @@ config.read(os.path.join(os.path.dirname(__file__), 'conflictMapper.cfg'))
 
 SLEEP_TIME = float(config.get('batch', 'SLEEP_TIME'))
 DATABASE_LOCATION = config.get('database', 'DATABASE_LOCATION')
+
 DEBUG_PORT = config.get('web server', 'DEBUG_PORT')
+WAIT_SECONDS_BEFORE_SHUTDOWN = float(config.get('web server',
+                                                'WAIT_SECONDS_BEFORE_SHUTDOWN'
+                                                ))
 SERVER_PORT = config.get('web server', 'SERVER_PORT')
